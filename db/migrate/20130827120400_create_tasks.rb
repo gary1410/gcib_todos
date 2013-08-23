07f702952b8b8e3630c2740a5993f631a47ac06f
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-23 14:04:15 -0700

    create schemas with migration

---
+++ db/migrate/20130827120400_create_tasks.rb
@@ -0,0 +1,10 @@
+require_relative '../config/application'
+
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :description
+      t.binary :complete
+    end
+  end
+end
